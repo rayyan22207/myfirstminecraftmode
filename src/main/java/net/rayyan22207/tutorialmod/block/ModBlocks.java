41f4c3b7a437262bed60d36198365b507b52be9f
rayyan22207 <rayyan22207@gmail.com>
2024-05-11 23:55:10 +0500

    first two recipes made

---
+++ src/main/java/net/rayyan22207/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,46 @@
+package net.rayyan22207.tutorialmod.block;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.rayyan22207.tutorialmod.TutorialMod;
+import net.rayyan22207.tutorialmod.item.Moditems;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    // DeferredRegister for blocks
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
+
+    // Registering blocks
+    public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
+            ()-> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+    public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block",
+            ()-> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+    // Method to register a block and its corresponding item
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    // Method to register a block item
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block){
+        return Moditems.ITEMS.register(name, ()-> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    // Method to register all blocks to the event bus
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+}
--- src/main/java/net/rayyan22207/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/rayyan22207/tutorialmod/item/ModCreativeModTabs.java
@@ -1,5 +1,33 @@
 package net.rayyan22207.tutorialmod.item;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.rayyan22207.tutorialmod.TutorialMod;
+import net.rayyan22207.tutorialmod.block.ModBlocks;
+
 public class ModCreativeModTabs {
 
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
+            ()-> CreativeModeTab.builder().icon(()-> new ItemStack(Moditems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(Moditems.SAPPHIRE.get());
+                        output.accept(Moditems.RAW_SAPPHIRE.get());
+
+                        output.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        output.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
 }
